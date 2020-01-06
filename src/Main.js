e2006d8313aaa349053980e7579f0bfced5f1b2d
Ryner Gabriel <drryner@icloud.com>
2020-01-06 03:47:44 +0300

    Add comments and simplify this programm

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,18 @@
+// A library to make HTTP requests with ease
 const request = require ("request")
+
+// A library to use host's file system
 const fs = require ("fs")
+
+// A library to use JavaScript's DOM to parse document
+// , probably it's better to use something more lightweight ?
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
-const ramda = require ("ramda")
-const R = ramda
 
+// A functional programming library
+const R = require ("ramda")
+
+// Horoscope representation
 class Horoscope {
     constructor (date, horoscopeSigns) {
         this.date = date
@@ -27,6 +35,7 @@ class Horoscope {
     }
 }
 
+// Horoscope sign representation as a handful data structure
 class HoroscopeSignProphecy {
     constructor (name, prophecyContent) {
         this.name = name
@@ -34,21 +43,40 @@ class HoroscopeSignProphecy {
     }
 }
 
-const generateHoroscope = (_error, _response, body) => {
+// A callback that parses horoscope file downloaded from stardm.com
+const horoschopeRequestHandler = (_error, _response, body) => {
     const doc = new JSDOM (body).window.document
-        , removeElementsWithUndefinedTag = xs => xs.filter (el => el.tagName !== undefined)
-        , rawProphecyArray = Array.from (doc.querySelector (".entry").childNodes)
-        , xs = removeElementsWithUndefinedTag (rawProphecyArray).slice (3). slice (0, -1).map (el => el.innerHTML)
-        , prophecises = R.splitEvery (2) (xs)
-    
-    const todayRaw = new Date()
-        , dd       = String(todayRaw.getDate()).padStart(2, '0')
-        , mm       = String(todayRaw.getMonth() + 1).padStart(2, '0')
-        , yyyy     = todayRaw.getFullYear()
-        , today    = yyyy + '-' + mm + '-' + dd
-        , horoscope = new Horoscope (today, prophecises.map (tuple => new HoroscopeSignProphecy (tuple[0], tuple[1])))
+    const removeElementsWithUndefinedTag = xs => xs.filter (el => el.tagName !== undefined)
+
+    // An entry class contains all signs and prophecies that we need
+    const prophecySourceArray = removeElementsWithUndefinedTag (Array.from (doc.querySelector (".entry").childNodes))
+
+    // Remove everything except for signs and prophecies
+    const prophecyArray = R.compose (
+        R.splitEvery (2), // Break down the array to chunks of 2
+        R.map (el => el.innerHTML), // map html tags to their content text
+        R.init, // Remove the first element
+        R.drop (3) // Remove last 3 elements
+    ) (prophecySourceArray)
+
+    // Get today's date in format YYYY-MM-DD H:M:S
+    const today = new Date ().toISOString ()
+                             .replace (/T/, ' ')
+                             .replace (/\..+/, '')
+
+    // Construct sign prophecies from pairs
+    const horoscopeSignProphecies =
+        prophecyArray.map (
+            pair => new HoroscopeSignProphecy (
+                R.head (pair),
+                R.last (pair)
+            )
+        )
+
+    const horoscope = new Horoscope (today, horoscopeSignProphecies)
 
     horoscope.saveTo ("test.txt")
 }
 
-request ("https://www.stardm.com/daily-horoscopes/A1-daily-horoscopes.asp", generateHoroscope)
+// Send HTTP request
+request ("https://www.stardm.com/daily-horoscopes/A1-daily-horoscopes.asp", horoschopeRequestHandler)
